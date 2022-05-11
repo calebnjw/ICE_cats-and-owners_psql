1f045dd102f481b59469a86cbc5555f8a3dd7b21
caleb ng <lelebng@gmail.com>
2022-05-11 20:33:53 +0800

    using SQL INNER JOIN instead of nested queries

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// SELECT * FROM [table] (WHERE [column]=[value])
-// SELECT * FROM [table] ORDER BY [column] ASC/DESC
-// INSERT INTO [table] ([column], [column]) VALUES ([value], [value])
-// UPDATE [table] SET [column]=[value] WHERE [column]=[value]
+// SELECT * FROM [table] (WHERE [column]=[value]);
+// SELECT * FROM [table] ORDER BY [column] ASC/DESC;
+// INSERT INTO [table] ([column], [column]) VALUES ([value], [value]);
+// UPDATE [table] SET [column]=[value] WHERE [column]=[value];
 // DELETE FROM [table] WHERE [column]=[value];
-// ALTER TABLE [table] CHANGE [column] [newName] [datatype]
+// ALTER TABLE [table] Change [column] [newName] [type];
 
 // import postgres
 import pg from 'pg';
@@ -97,92 +97,62 @@ if (action === 'create-cat') {
 }
 
 if (action === 'cats') {
-  const ownerQuery = 'SELECT * FROM cat_owners';
-  pool.query(ownerQuery, (ownerError, ownerResult) => {
-    console.log('Getting Owners');
+  const query = `SELECT cats.cat_name, cats.owner_id, cat_owners.id, cat_owners.owner_name 
+  FROM cats
+  INNER JOIN cat_owners
+  ON cats.owner_id=cat_owners.id;`;
 
-    if (ownerError) {
-      console.log(ownerError);
+  pool.query(query, (error, result) => {
+    console.log('Getting Cats');
+
+    if (error) {
+      console.log(error);
       return;
     }
 
-    // create new object to store names and id
-    // id will be the key, and name will be value
-    // this is so that we can substitute the value into the cat lookup
-    const owners = {};
-    ownerResult.rows.forEach((owner) => {
-      owners[owner.id] = owner.name;
-    });
-
-    const catQuery = 'SELECT * FROM cats';
-    pool.query(catQuery, (catError, catResult) => {
-      console.log('Getting Cats');
-
-      if (catError) {
-        console.log(catError);
-        return;
-      }
-
-      console.log('Cats:');
-      catResult.rows.forEach((cat) => {
-        // getting name and owner_id of the cat
-        const { name: catName, owner_id: catOwnerID } = cat;
-        // getting name of cat owner using the object we created above
-        const catOwner = owners[catOwnerID];
-
-        const output = `${catName}: ${catOwner}`;
-        console.log(output);
-      });
+    result.rows.forEach((cat) => {
+      console.log(`${cat.cat_name}: ${cat.owner_name}`);
     });
   });
 }
 
 if (action === 'owners') {
-  const catQuery = 'SELECT * FROM cats';
-  pool.query(catQuery, (catError, catResult) => {
-    console.log('Getting Cats');
+  const query = `SELECT cats.cat_name, cats.owner_id, cat_owners.id, cat_owners.owner_name 
+  FROM cats
+  INNER JOIN cat_owners
+  ON cats.owner_id=cat_owners.id;`;
 
-    if (catError) {
-      console.log(catError);
+  pool.query(query, (error, result) => {
+    console.log('Getting Owners');
+
+    if (error) {
+      console.log(error);
       return;
     }
 
     // new object to store cats grouped by owner_id
-    // owner_id is the key, and the value will be an array with cat names
+    // owner_name is the key, value will be an array with cat names
     const catsByOwner = {};
-    catResult.rows.forEach((cat) => {
+    result.rows.forEach((cat) => {
       // if the owner is not registered in the array,
-      if (catsByOwner[cat.owner_id] === undefined) {
+      if (catsByOwner[cat.owner_name] === undefined) {
         // create new key:value pair, with the value stored in an array
-        catsByOwner[cat.owner_id] = [cat.name];
+        catsByOwner[cat.owner_name] = [cat.cat_name];
       } else {
         // else append cat name to the end of the existing list
-        catsByOwner[cat.owner_id].push(cat.name);
+        catsByOwner[cat.owner_name].push(cat.cat_name);
       }
     });
 
-    const ownerQuery = 'SELECT * FROM cat_owners';
-    pool.query(ownerQuery, (ownerError, ownerResult) => {
-      console.log('Getting Owners');
+    // get the list of owners from the object
+    const owners = Object.keys(catsByOwner);
 
-      if (ownerError) {
-        console.log(ownerError);
-        return;
-      }
-
-      console.log('Owners:');
-      // ownerResult.rows should return a list of objects
-      ownerResult.rows.forEach((owner) => {
-        // getting name and id of the owners
-        const { name: ownerName, id: ownerID } = owner;
-
-        console.log(`- ${ownerName}`);
-
-        // then we make use of the object we created above
-        // to get the list of cats from each owner using owner id
-        catsByOwner[ownerID].forEach((cat) => {
-          console.log(`  - ${cat}`);
-        });
+    // iterate through each owner
+    owners.forEach((owner) => {
+      console.log(`- ${owner}`);
+      // iterate through each array to get cat
+      catsByOwner[owner].forEach((cat) => {
+        console.log(`  - ${cat}`);
       });
     });
   });
